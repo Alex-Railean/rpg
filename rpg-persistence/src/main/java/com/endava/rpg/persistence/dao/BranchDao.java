48132026b5269c1c268114ece37e80bb1c18228e
Alexandru Railean <Alexandru.Railean@endava.com>
2018-03-22 12:00:52 +0200

    Added UnsupportedOperationException() to unsupported BranchDao methods

--- rpg-persistence/src/main/java/com/endava/rpg/persistence/dao/BranchDao.java
+++ rpg-persistence/src/main/java/com/endava/rpg/persistence/dao/BranchDao.java
@@ -5,11 +5,29 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class BranchDao extends Dao<BranchEntity> {
 
     @Autowired
     private BranchDao(SessionFactory sessionFactory) {
         super(BranchEntity.class, sessionFactory);
+    }
+
+    public <V> BranchEntity getSingleWhere(String columnName, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public <V> List<BranchEntity> getAllWhere(String columnName, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<BranchEntity> getAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean deleteAll() {
+        throw new UnsupportedOperationException();
     }
 }
